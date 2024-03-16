87e45e419de75e29bac4693aa4789241f9308a13
Bartosz Nowak <bartekn337@gmail.com>
2024-03-16 15:21:29 +0100

    buildrs update

--- barretenberg/rs/build.rs
+++ barretenberg/rs/build.rs
@@ -7,9 +7,21 @@ fn main() {
     // Build the C++ code using CMake and get the build directory path.
     let dst = Config::new("../cpp")
         .profile("RelWithAssert")
-        .define("TARGET_ARCH", "skylake")
-        .configure_arg("--preset=clang16")
-        .configure_arg("--toolchain=../cmake/toolchains/x86_64-linux.cmake")
-        .build_target("bb")
+        .define("TARGET_ARCH", "x86-64")
+        .configure_arg("--toolchain=cmake/toolchains/x86_64-linux.cmake")
+        .build_target("barretenberg")
         .build();
+
+    // Add the library search path for Rust to find during linking.
+    println!("cargo:rustc-link-search={}/lib", dst.display());
+
+    // Link the `barretenberg` static library.
+    println!("cargo:rustc-link-lib-static=barretenberg");
+
+    // Link the C++ standard library.
+    if cfg!(target_os = "macos") || cfg!(target_os = "ios") {
+        println!("cargo:rustc-link-lib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
 }
